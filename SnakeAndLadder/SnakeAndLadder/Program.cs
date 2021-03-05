27d09b2d1665880e78bd21d756816edecea6b8ff
Mansi-Atey <mansiatey31@gmail.com>
2021-03-05 18:00:33 +0530

    [mansi]ADD: snake and ladder

---
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        public const int startPosition = 0;
+        public const int endPosition = 100;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME..!! Please Enter Your Name.");
+            string user1 = Console.ReadLine();
+            int userPosition = startPosition;
+            Console.WriteLine("Your Current Position is:" + userPosition);
+        }
+    }
+}
